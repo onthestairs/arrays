702ec1bb22c813d2d47b444b29a3772f3c9443a5
Austin Platt <austinplatt@gmail.com>
2024-12-01 11:01:50 +0000

    upgrade uiua

--- flake.nix
+++ flake.nix
@@ -8,14 +8,14 @@
     with flake-utils.lib; eachSystem allSystems (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        my-uiua = pkgs.callPackage ./uiua.nix { };
       in
       {
-
         # defaultPackage = packages.document;
         devShell = pkgs.mkShell {
           # development environment
           buildInputs = [
-            pkgs.uiua
+            my-uiua
           ];
         };
       });
